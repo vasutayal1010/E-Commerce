c7a539228edf8d419a2890ccbb4b6ab7ff2d267c
Vasu Tayal <vasutayal1010@gmail.com>
2024-07-11 09:48:29 +0530

    Login/Logout

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
+import Register from './pages/Auth/Register';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
+import ForgotPassword from './pages/Auth/ForgotPassword';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
